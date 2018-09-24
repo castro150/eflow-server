4ab281220c62931980bfe7fd58b49480b0709617
castro150 <rafaelcarneiroget@hotmail.com>
2018-09-24 17:17:05 -0300

    Adjusting save user test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 let express = require('express');
 let router = express.Router();
 
+const mongoose = require('mongoose');
+const User = mongoose.model('Contact');
+
 // GET users listing.
 router.get('/', function(req, res) {
-	res.send('respond with a resource');
+	// TODO remove: just to test
+	let contact = new User({ name: 'Rafael' });
+	console.log(contact);
+	contact.save(function (err, contact) {
+		if (err) return console.error(err);
+		console.log('Saved contact: ', contact);
+		res.send('respond with a resource');
+	});
 });
 
 module.exports = router;
